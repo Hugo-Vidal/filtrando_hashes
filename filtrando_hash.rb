96ebc416e3847de86b381b04f5f8724971fc2bbf
Hugo-Vidal <hugo.f.vidal.r@gmail.com>
2019-04-20 19:34:02 -0400

    filtrando hashes

---
+++ filtrando_hash.rb
@@ -0,0 +1,12 @@
+ventas = {
+    Octubre: 65000,
+    Noviembre: 68000,
+    Diciembre: 72000
+    }
+nuevas_ventas = {}
+
+ventas.each do |k,v|
+    nuevas_ventas[k] = v if v < 70000
+end
+
+puts nuevas_ventas
